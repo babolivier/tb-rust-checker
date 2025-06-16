2a8eb9ef96711af73336f625f77597328330fb90
Brendan Abolivier <brendan@thunderbird.net>
2025-06-16 12:28:08 +0100

    Improve formatting for errors coming from reqwest
    reqwest's default error formatting can hide some important information required
    to fix bugs (such was what in a response made it fail to parse). So we need to
    be a bit more thorough when formatting those for display if we want error logs
    to be actually helpful.

--- src/error.rs
+++ src/error.rs
@@ -2,6 +2,8 @@
  * License, v. 2.0. If a copy of the MPL was not distributed with this
  * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
 
+use std::error::Error as StdError;
+use std::fmt::Display;
 use std::io;
 
 use thiserror::Error;
@@ -9,15 +11,43 @@ use thiserror::Error;
 /// Errors that happen after the bot's initial setup.
 #[derive(Error, Debug)]
 pub(crate) enum Error {
-    #[error("network error: {0}")]
     Network(#[from] reqwest::Error),
-
-    #[error("I/O error: {0}")]
     Io(#[from] io::Error),
-
-    #[error("JSON (de)serialization error: {0}")]
     Json(#[from] serde_json::Error),
-
-    #[error("URL parse error: {0}")]
     UrlParse(#[from] url::ParseError),
+}
+
+impl Display for Error {
+    // We don't implement `Display` using thiserror's `error` attributes,
+    // because we need some extra processing to properly handle reqwest errors.
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        match self {
+            Error::Network(error) => {
+                write!(f, "network error: {error}")?;
+
+                // If the error has a source, then we want to include it as
+                // well, because reqwest's error itself might not include any
+                // helpful data. For example, a deserialization error from serde
+                // would be displayed as just "error decoding response body",
+                // rather than anything indicating what's wrong with the
+                // response body.
+                match error.source() {
+                    Some(source) => write!(f, ": {source}")?,
+                    None => {}
+                };
+
+                // If there's a URL attached to the error, then also include it
+                // so the logs can show what kind of request was being performed
+                // when the error occurred.
+                if let Some(url) = error.url() {
+                    write!(f, " (URL: {})", url.as_str())?;
+                }
+
+                Ok(())
+            }
+            Error::Io(error) => write!(f, "I/O error: {error}"),
+            Error::Json(error) => write!(f, "JSON (de)serialization error: {error}"),
+            Error::UrlParse(error) => write!(f, "URL parse error: {error}"),
+        }
+    }
 }
