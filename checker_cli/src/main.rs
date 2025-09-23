5fcdea7f1db7a1cf1829d5cbf5f8dfbf4cb0f61b
Brendan Abolivier <brendan@thunderbird.net>
2025-09-23 15:07:18 +0100

    Add a CLI to manually check checksums against a set of revisions

---
+++ checker_cli/src/main.rs
@@ -0,0 +1,47 @@
+/* This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
+
+use std::env;
+
+use clap::Parser;
+
+use checker_base::checksums::{ChangeSet, verify_checksums_match};
+
+#[derive(Parser, Debug)]
+struct Args {
+    /// The mozilla-unified revision to use. Defaults to "central".
+    #[arg(short, long)]
+    mozilla_rev: Option<String>,
+
+    /// The comm-unified revision to use. Defaults to "comm".
+    #[arg(short, long)]
+    comm_rev: Option<String>,
+}
+
+impl From<Args> for ChangeSet {
+    fn from(value: Args) -> Self {
+        ChangeSet {
+            moz_rev: value.mozilla_rev,
+            tb_rev: value.comm_rev,
+        }
+    }
+}
+
+#[tokio::main(flavor = "current_thread")]
+async fn main() {
+    let args = Args::parse();
+
+    env_logger::init();
+
+    // Hack to set the default log level to "info".
+    if env::var("RUST_LOG").is_err() {
+        // SAFETY: We're always running this program in a single-thread context.
+        unsafe { env::set_var("RUST_LOG", "info") }
+    }
+
+    match verify_checksums_match(args.into()).await {
+        Ok(ok) => log::info!("checksums match: {ok}"),
+        Err(err) => log::error!("error while verifying files: {err}"),
+    }
+}
